32fbdb4391d34e4f84a2fc8154843aa41c6683d3
mac <mac@macdeMacBook-Pro-61.local>
2016-11-22 17:58:46 +0800

    11

---
+++ dsaf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dsaf
+//
+//  Created by mac on 16/11/22.
+//  Copyright (c) 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dsaf/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  dsaf
+//
+//  Created by mac on 16/11/22.
+//  Copyright (c) 2016年 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    int a = 0;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
